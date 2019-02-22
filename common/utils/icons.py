7005e05f59df29253f3a876d37d6659f5aa469b9
DeathAxe <deathaxe@googlemail.com>
2019-02-22 23:50:26 +0100

    Fix issue with certain themes
    This commit fixes an issue with themes, which are marked with `.supports-a-file-icon-customization` but do not provide own icons.

--- common/utils/icons.py
+++ common/utils/icons.py
@@ -14,18 +14,17 @@ def get_path(package_name):
 
 
 def get_missing(theme_package):
-    missing_icons = []
-    all_icons = sublime.find_resources("*.png")
     package_icons = json.loads(sublime.load_resource("Packages/" +
                                                      settings.PACKAGE_NAME +
                                                      "/common/icons.json"))
     theme_icons_path = get_path(theme_package)
-    theme_icons = [
-        os.path.basename(os.path.splitext(i)[0])
-        for i in all_icons if i.startswith(theme_icons_path)
-    ]
-    for icon in package_icons:
-        if icon not in theme_icons:
-            missing_icons.append(icon)
+    if not theme_icons_path:
+        return package_icons
 
-    return missing_icons
+    theme_icons = {
+        os.path.basename(os.path.splitext(i)[0])
+        for i in sublime.find_resources("*.png")
+        if i.startswith(theme_icons_path)
+    }
+
+    return [icon for icon in package_icons if icon not in theme_icons]
