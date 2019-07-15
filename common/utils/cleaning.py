d63fef179b0a657c98bc3f7f303b389009789989
DeathAxe <deathaxe@googlemail.com>
2019-07-15 19:37:43 +0200

    Refactor cleaning module
    Make use of shutil's features to not stop cleaning at the first error.

--- common/utils/cleaning.py
+++ common/utils/cleaning.py
@@ -1,70 +1,62 @@
-import glob
 import os
 import shutil
+
 import sublime
 import sublime_plugin
 
 from . import path
-from .. import settings
 
 from .logging import log, dump, message
 
 
-def _get_patches(dir, pattern):
-    for dirname, subdirs, files in os.walk(dir):
-        for f in files:
-            if f.endswith(pattern):
-                yield os.path.join(dirname, f)
-
-
 def clean_all():
     message("Cleaning up")
-    overlay_path = path.get_overlay()
 
-    if os.path.exists(overlay_path):
-        try:
-            shutil.rmtree(overlay_path)
-        except Exception as error:
+    success = True
+
+    def _on_rmtree_error(function, path, excinfo):
+        if success:
+            success = False
             log("Error during cleaning")
-            dump(error)
-        else:
-            message("Cleaned up successfully")
+        dump(error)
+
+    shutil.rmtree(path.get_overlay_cache(), onerror=_on_rmtree_error)
+    shutil.rmtree(path.get_overlay(), onerror=_on_rmtree_error)
+
+    if success:
+        message("Cleaned up successfully")
+
+    return success
 
 
-def clean_patches():
+def clean_theme_patches():
     log("Cleaning patches")
-    overlay_path = path.get_overlay()
-    patches = _get_patches(overlay_path, ".sublime-theme")
 
-    if os.path.exists(overlay_path):
-        try:
-            for p in patches:
-                os.remove(p)
-        except Exception as error:
-            log("Error during cleaning")
-            dump(error)
-        else:
-            log("Cleaned up successfully")
-            sublime.run_command("afi_patch_themes")
+    success = True
+
+    for dirname, _, files in os.walk(path.get_overlay()):
+        for f in files:
+            if f.endswith(".sublime-theme"):
+                try:
+                    os.remove(os.path.join(dirname, f))
+                except Exception as error:
+                    if success:
+                        success = False
+                        log("Error during cleaning")
+                    dump(error)
+    if success:
+        log("Cleaned up successfully")
+        sublime.run_command("afi_patch_themes")
 
 
 def revert():
-    log("Reverting to a freshly installed state")
-
-    try:
-        shutil.rmtree(path.get_overlay_cache(), ignore_errors=True)
-        shutil.rmtree(path.get_overlay(), ignore_errors=True)
-    except Exception as error:
-        log("Error during reverting")
-        dump(error)
-    else:
-        log("Reverted successfully")
+    if clean_all():
         sublime.run_command("afi_reload")
 
 
 class AfiCleanCommand(sublime_plugin.ApplicationCommand):
     def run(self):
-        sublime.set_timeout_async(clean_patches)
+        sublime.set_timeout_async(clean_theme_patches)
 
 
 class AfiRevertCommand(sublime_plugin.ApplicationCommand):
