lapin00b/AFileIcon

commit ab1def9e48a23e278463a223906d6964aea29ccb

Apply icon size setting to specific themes
Fixes #54

This commit adds support to apply `"size": ...` setting on themes which
are marked to support A File Icon.

core/themes.py
@@ -156,7 +156,13 @@ def _create_specific_patch(settings):
     theme_content = []
 
     color = convert_color_value(settings.get("color"))
-    theme_content.append(_patch_icon(None, color))
+    icon = _patch_icon(None, color)
+
+    size = settings.get("size")
+    if size:
+        icon["content_margin"] = [size, size]
+
+    theme_content.append(icon)
 
     if color:
         color_on_hover = convert_color_value(settings.get("color_on_hover"))
@@ -172,7 +178,9 @@ def _create_specific_patch(settings):
 
 
 def _patch_icon(attrib, color=None, opacity=None):
-    icon = {"class": "icon_file_type", "layer0.tint": color}
+    icon = {"class": "icon_file_type"}
+    if color:
+        icon["layer0.tint"] = color
     if attrib:
         icon["parents"] = [{"class": "tree_row", "attributes": [attrib]}]
     if opacity:

commit 39fed5fc0e2a4c97e8fe0be5dcdbb1d77a55070b

Fix icons tinted black in Default.sublime-theme
Fixes #60

The default theme tints file icons. Thus this rule needs to be cleared
to enable colorful icons. The original issue #29 was fixed but
unfortunately regressed by ab1def9e48a23e278463a223906d6964aea29ccb.

This commit partly reverts the regressing commit and adds a comment to
ensure it doesn't happen again.

core/themes.py
@@ -178,11 +178,20 @@ def _create_specific_patch(settings):
 
 
 def _patch_icon(attrib, color=None, opacity=None):
-    icon = {"class": "icon_file_type"}
-    if color:
-        icon["layer0.tint"] = color
+    """Built an icon theme rule
+
+    :attrib:
+        The tree row's state attribute the returned rule applies to.
+    :color:
+        The color to tint the icon with.
+        It is always required to override any theme settings. The default value of
+        `none` removes any tinting specified by a theme to display colored icons.
+    :opacity:
+        If specified overwrites theme's opacity setting.
+    """
+    icon = {"class": "icon_file_type", "layer0.tint": color}
     if attrib:
         icon["parents"] = [{"class": "tree_row", "attributes": [attrib]}]
-    if opacity:
+    if opacity is not None:
         icon["layer0.opacity"] = opacity
     return icon